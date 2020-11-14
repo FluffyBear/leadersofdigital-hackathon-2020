43747426c382c8991473bbdaf072b59c8abcc1b4
Aleksandr Maksimenko <xe,frrjdfybT8>
2020-11-14 15:05:27 +0300

    big data test

--- src/test/kotlin/rosatom/Graphs.kt
+++ src/test/kotlin/rosatom/Graphs.kt
@@ -2,14 +2,14 @@ package rosatom
 
 object Graphs {
     val simpleGraph = Project(
-        listOf(
-            Job(1, "1", 1, 2),
-            Job(2, "2", 1, 2),
-            Job(3, "3", 1, 1),
-            Job(4, "4", 1, 2),
-            Job(5, "5", 3, 2),
-            Job(6, "6", 3, 3),
-            Job(7, "7", 6, 1)
+        mutableMapOf(
+            Pair(1,Job(1, "1", 1, 2)),
+            Pair(2,Job(2, "2", 1, 2)),
+            Pair(3,Job(3, "3", 1, 1)),
+            Pair(4,Job(4, "4", 1, 2)),
+            Pair(5,Job(5, "5", 3, 2)),
+            Pair(6,Job(6, "6", 3, 3)),
+            Pair(7,Job(7, "7", 6, 1))
         ),
         listOf(
             1 to 2,
@@ -19,12 +19,12 @@ object Graphs {
     )
 
     val validGraph = Project(
-        listOf(
-            Job(1, "1", 1, 2),
-            Job(2, "2", 4, 2),
-            Job(3, "3", 4, 2),
-            Job(4, "4", 7, 2),
-            Job(5, "5", 10, 2)
+        mutableMapOf(
+            Pair(1,Job(1, "1", 1, 2)),
+            Pair(2,Job(2, "2", 4, 2)),
+            Pair(3,Job(3, "3", 4, 2)),
+            Pair(4,Job(4, "4", 7, 2)),
+            Pair(5,Job(5, "5", 10, 2))
         ),
         listOf(
             1 to 2,
@@ -36,11 +36,11 @@ object Graphs {
     )
 
     val badGraph = Project(
-        listOf(
-            Job(1, "1", 1, 2),
-            Job(2, "2", 3, 2),
-            Job(3, "3", 3, 2),
-            Job(4, "4", 1, 2)
+        mutableMapOf(
+            Pair(1,Job(1, "1", 1, 2)),
+            Pair(2,Job(2, "2", 3, 2)),
+            Pair(3,Job(3, "3", 3, 2)),
+            Pair(4,Job(4, "4", 1, 2))
         ),
         listOf(
             1 to 2,
--- src/test/kotlin/rosatom/TestGeneration.kt
+++ src/test/kotlin/rosatom/TestGeneration.kt
@@ -16,13 +16,13 @@ class TestGeneration {
     @ImplicitReflectionSerializer
     @Test
     fun generate() {
-        File("src/main/resources/test/3.json").writeText(Json(JsonConfiguration.Stable)
+        File("src/main/resources/analyse-test/3.json").writeText(Json(JsonConfiguration.Stable)
             .stringify(AnalyseRequestDto(generateJobs(),generateDependencies())))
     }
 
     private fun generateJobs(): List<SimplifiedJobDto> {
         val jobs = mutableListOf<SimplifiedJobDto>()
-        for (i in 1..1000 step 10) {
+        for (i in 1..990 step 10) {
             jobs.add(SimplifiedJobDto(i+0, "${i+0}", 1, 2))
             jobs.add(SimplifiedJobDto(i+1, "${i+1}", 4, 4))
             jobs.add(SimplifiedJobDto(i+2, "${i+2}", 3, 1))
@@ -34,10 +34,23 @@ class TestGeneration {
             jobs.add(SimplifiedJobDto(i+8, "${i+8}", 5, 3))
             jobs.add(SimplifiedJobDto(i+9, "${i+9}", 12, 1))
         }
-        for (i in 100 until 1001 step 100) {
+        jobs.add(SimplifiedJobDto(991, "${991+0}", 1, 2))
+        jobs.add(SimplifiedJobDto(992, "${991+1}", 2, 4))
+        jobs.add(SimplifiedJobDto(993, "${991+2}", 2, 1))
+        jobs.add(SimplifiedJobDto(994, "${991+3}", 4, 4))
+        jobs.add(SimplifiedJobDto(995, "${991+4}", 3, 4))
+        jobs.add(SimplifiedJobDto(996, "${991+5}", 7, 2))
+        jobs.add(SimplifiedJobDto(997, "${991+6}", 9, 3))
+        jobs.add(SimplifiedJobDto(998, "${991+7}", 10, 2))
+        jobs.add(SimplifiedJobDto(999, "${991+8}", 5, 3))
+        jobs.add(SimplifiedJobDto(1000, "${991+9}", 12, 1))
+        for (i in 100 until 1001 step 200) {
             jobs.add(SimplifiedJobDto(1000 + i/100, "${1000 + i/100}", 15, 2))
         }
-        jobs.add(SimplifiedJobDto(1011, "1011", 16, 2))
+        for (i in 200 until 1001 step 200) {
+            jobs.add(SimplifiedJobDto(1000 + i/100, "${1000 + i/100}", 15, 1))
+        }
+        jobs.add(SimplifiedJobDto(1011, "1011", 15, 2))
         return jobs
     }
 
